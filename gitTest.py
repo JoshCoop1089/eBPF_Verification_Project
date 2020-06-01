5e2a1b289c041018407092e7b84dbdbbaa956a8d
Josh Cooper <joshcoop1089@gmail.com>
2020-06-01 18:17:37 -0400

    z3 code addition

--- gitTest.py
+++ gitTest.py
@@ -3,6 +3,18 @@
 Created on Mon Jun  1 17:32:08 2020
 
 @author: joshc
+
+Code Examples from:
+    https://theory.stanford.edu/~nikolaj/programmingz3.html
 """
 
 
+from z3 import *
+
+Tie, Shirt = Bools('Tie Shirt')
+s = Solver()
+s.add(Or(Tie, Shirt), 
+      Or(Not(Tie), Shirt), 
+      Or(Not(Tie), Not(Shirt)))
+print(s.check())
+print(s.model())

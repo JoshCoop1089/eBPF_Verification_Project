22f67fc9ee82dedd9b23d2a54686cd898dcea4a8
Josh Cooper <joshcoop1089@gmail.com>
2020-06-02 12:38:20 -0400

    Beginning tests of 205 Psets

---
+++ Josh Code Tests/z3Tests/205 Pset 1.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  2 11:47:47 2020
+
+@author: joshc
+"""
+
+
+"""
+Using PSet 1 Problems from 205 as a base for practicing z3 functions
+"""
+
+'''
+Problem 4:
+    
+    a = User can access file system
+    s = User can save
+    u = System being upgraded
+    
+    1) u => ~a
+    2) a => s
+    3) ~s => ~u
+    
+    According to HW, U = True, A = False, S = True satisfies the given params.    
+'''
+from z3 import *
+a, s, u = Bools('a s u')
+cond1 = Implies(u, Not(a))
+cond2 = Implies(a,s)
+cond3 = Implies(Not(s), Not(u))                
+s = Solver()
+s.add(cond1,cond2,cond3)
+print(s.check())
+print(s.model())
+

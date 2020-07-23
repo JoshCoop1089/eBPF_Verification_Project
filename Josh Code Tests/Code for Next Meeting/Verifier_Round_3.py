67c68e13eb4b664bb215eea87a8680ad7148954d
Josh Cooper <joshcoop1089@gmail.com>
2020-07-22 23:37:15 -0400

    Branching works! But the branch does wonky calculations!

--- Josh Code Tests/Code for Next Meeting/Verifier_Round_3.py
+++ Josh Code Tests/Code for Next Meeting/Verifier_Round_3.py
@@ -395,117 +395,6 @@ def mov_to_reg(input_value, target_reg, register_state_helper, destination_reg, 
 
     return mov_function, r_s_h
 
-# Jump Instructions  (this won't be needed anymore, as the idea for jump is just to return a signal to split off a new branch)
-def jump_general(input_value, target_reg, offset, register_state_helper, destination_reg, extension_length):
-    """
-    Thoughts on how to operate the jump instruction
-    
-    Jump creates a defined branch point, where register values should be frozen and 
-    referenced in case something goes awry
-    
-    Can we use an offset marker to pass into the jump function, and do something like.
-    
-    if == then implies next_instruction
-    if != then implies next_instruction + offset?
-    
-    
-    If i pass in the instruction list with reg_state_helper, I can use it to create implication 
-        statements based on the value of offset
-        
-    Example Situation:
-        jneR with offset of 3, and r0, r1 comparison, is located at instruction 4:
-        
-            this means that if r0 = r1, instruction 5 is implied
-            else, if r0 != r1, instruction 8 is implied
-            
-        Since we cannot assume instruction 8 is the last instruction, we need to add 
-            the smaller branch (ins 5, 6, and 7) to the solver, and then return to 
-            execute instruction 9 if needed in the main program
-            
-    Current plan:
-        1) Get the values to be compared
-        2) Set up the implication statements for positive and negative results on comparison
-            by using create_new_constraint on the single needed instructions
-        3) Add the positive implication to the solver and check it.
-        3) Snip a portion of instruction list, and pass it into execute_branch.
-        4) Execute_branch will act like normal execute_program, moving through the
-            instruction sublist, except it will return register_state_helper when completed.
-        5) if execute branch returns without a problem, add the negative implication
-           
-    Problem:
-        how to reference the right instances of a register for the second implication if it is changed in the branch
-        
-    Possible solution:
-        create a small array of the last entries in each register list to reference for neg implication?
-    """
-    r_s_h = register_state_helper
-    
-    list_of_locations, r_s_h = get_the_locations_and_extend(input_value, target_reg, r_s_h, destination_reg, extension_length)
-    
-    # I don't feel like rewriting get loc and extend to not always add a new reg value on the 
-    # destination register list, so i'll just delete it here
-    del r_s_h.register_history[target_reg][-1]
-    
-    # list_of_locations = [source_val, destination_old_val, destination_new_val]
-    comparison_statement = list_of_locations[0] == list_of_locations[1]
-    
-    
-    # If comparison is true: proceed through rest of instruction list
-    # If comparison is false, jump to offset instruction and proceed through rest of program
-    
-    # Need to make a copy of the current solver to direct new creation of dual branches
-    
-    """
-    JNE isn't passing back any new information, jne is passing back the problem flag to tell the main executor 
-        that it needs to initalize a new branch object'
-    
-    """
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # Getting the most recent instances of all register values to compare against
-    current_reg_states = [r_s_h.register_history[i][-1] for i in range(r_s_h.num_Regs)]
-   
-    for i in current_reg_states:
-        print(i.name)
-        
-    # These two variables should hold the index of the required instructions
-    next_ins = r_s_h.instruction_number + 1
-    next_ins_with_offset = r_s_h.instruction_number + 1 + offset
-
-    equal_next_ins , r_s_h = \
-        create_new_constraints_based_on_instruction(r_s_h.instruction_list[next_ins], register_state_helper)
-    
-    # formulas returned from create_new_constraints
-    comparison_equal_implication = Implies(comparison_statement, equal_next_ins)
-    print(comparison_equal_implication)
-    print()
-    
-    branch_of_program = r_s_h.instruction_list[next_ins:next_ins_with_offset]
-    print(branch_of_program)
-    for instruction_number,instructon in enumerate(branch_of_program,r_s_h.instruction_number):
-        a=b
-    
-    not_equal_next_ins , r_s_h = \
-        create_new_constraints_based_on_instruction(r_s_h.instruction_list[next_ins_with_offset], register_state_helper)
-    comparison_not_equal_implication = Implies(Not(comparison_statement), not_equal_next_ins)
-    print(comparison_not_equal_implication)
-    
-    
-    # comparison_not_equal_implication = Implies(Not(comparison_statement), next_ins_with_offset)
-    comparison_constraints = comparison_statement
-    
-    
-    
-    return comparison_constraints, register_state_helper
-
 def exit_instruction(register_state_helper):
     exit_ins = Bool("exit_%d"%register_state_helper.instruction_number)
     return exit_ins, register_state_helper 
@@ -949,62 +838,6 @@ def execute_program_v3(all_branches):
     for branch in all_branches.branch_list:
         check_and_print_model(branch.instruction_list, branch)
         
-def execute_program_v2(program_list, register_state_helper):
- 
-    # Add instructions from the program list to the solver
-    for instruction_number, instruction in enumerate(program_list):
-        
-        register_state_helper.instruction_number = instruction_number
-        
-        # This should allow skipping forward in the ins list without adding skipped instructions
-        if register_state_helper.problem_flag > instruction_number:
-            continue
-        
-        print("Attempting to combine solver with instruction #%d: %s"%(instruction_number, instruction))
-        new_constraints, register_state_helper = \
-            create_new_constraints_based_on_instruction(instruction, register_state_helper)
-        
-        # Debug just in case
-        # print(register_state_helper)
-        # v2 Plan, move correctness checking into execute_program main functions
-        
-        #Allow for rollback in event of problematic addition
-        register_state_helper.solver_object.push()
-        
-        # Finally put the constraints from the instruction into the solver
-        register_state_helper.solver_object.add(new_constraints)
-                
-        #Always check a solution before returning to the main function
-        if register_state_helper.solver_object.check() == unsat:
-            
-            # Roll back the solver to a version before the problematic instructions
-            register_state_helper.solver_object.pop()
-        
-            # Special return value to tell the main test program that an error has occured
-            register_state_helper.problem_flag = register_state_helper.instruction_number * -1
-            
-            # Corner case if it fails on the first instruction (ie instruction 0)
-            if register_state_helper.instruction_number == 0:
-                register_state_helper.problem_flag = -1
-            
-        else:
-            register_state_helper.problem_flag = instruction_number
-        
-        # In case you want to see the register values after every instruction
-        print_current_register_state(register_state_helper)
-        # Special register_state_helper.problem_flag returns:
-            # problem_flag < 0 --> an instruction caused an unsat solution
-            # problem_flag > instruction_counter --> a jump instruction is pushing the list forward
-        
-        # Problem_flag < 0 means that a specific instruction caused a problem, 
-        # and we're exiting without finishing all the instructions in the program
-        if register_state_helper.problem_flag < 0:
-            print("\nThe program encountered an error on instruction #%s"%abs(register_state_helper.problem_flag))
-            print("\t-->  " + program_list[abs(register_state_helper.problem_flag)] + "  <--")
-            print("The last viable solution before the problem instruction is shown below:")
-            break
-
-    check_and_print_model(program_list, register_state_helper)
     
 def create_program(program_list = ""):
     """
@@ -1078,6 +911,4 @@ def create_program(program_list = ""):
     
     execute_program_v3(all_branches)
     
-
-
 create_program()

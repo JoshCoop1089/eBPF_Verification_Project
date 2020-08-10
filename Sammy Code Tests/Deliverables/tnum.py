56e6aba62bd5c3f97f5f8b1b3f097bd333298a16
1pwny <sammy27berger@gmail.com>
2020-08-10 13:02:52 -0400

    tnum_in works

--- Sammy Code Tests/Deliverables/tnum.py
+++ Sammy Code Tests/Deliverables/tnum.py
@@ -32,6 +32,22 @@ class tnum:
     def __str__(self):
         return "<min: " + str(self.min) + ", range: " + str(self.range) + ">"
 
+
+    #when the sat solver generates tnums we have to ensure it complies with good tnum practices
+    def validate(self):
+        """
+        intended result: every 1-bit of self.range is a 0-bit in self.min
+
+        plan - self.min == self.min & ~self.range
+
+        Should work. If any unknown bits are set to 1, then ~self.range will be a 0, the result after the & will be 0, and the
+        two values will not be equivalent. Any known value is a 1 in self.range, so they will be unchanged by the &
+        """
+
+        unknownCheck = self.min & ~self.range
+        return self.min == unknownCheck
+        
+    
     def lshift(self, i):
         LShL(self.mini, i)
         LShL(self.range, i)
@@ -46,7 +62,7 @@ class tnum:
     def eq_const(self, c):
         return And(self.is_const(), self.min == c)
 
-    def eq_tnum(self, other):
+    def tnum_eq(self, other):
         return And(self.min == other.min, self.range == other.range)
 
     def add(self, other):
@@ -110,11 +126,37 @@ class tnum:
         mu = self.range & other.range
         return tnum(v & ~mu, mu)
 
+    #should check whether 'other' is a subset of 'self'
     def tnum_in(self, other):
+        """
         if (other.range & ~self.range) is not 0:
             return False
+
+            > Requirement to return True: other.range & ~self.range == 0
+            
+            > Conceptually: Ensures no overlap between known bits of self and unknown bits of other. This means that every variable
+            bit in other is also variable in self.
+        
         other.min &= ~self.range
+
+            > Conceptually: If the bit is unknown in self, then it will be 0 in ~self.range. This will result in a 0 after the '&'
+            operation for every unknown bit. Known bits in self will be 1 in ~self.range; thus, all bits in other.min at known
+            locations will be propagated forward (0 & 1 = 0, 1 & 1 = 1)
+        
         return self.min == other.min
+
+            > Requirement to return True: self.min == other.min & ~self.range
+            
+            > Conceptually: Look at previous step. If the bit is unknown in self.range, it will always be set to 0. This is what
+            we require from the tnum.min field. If th bit is known in self.range, it will be set to its value and compared against
+            the value in self.min. If there's a discrepancy, then other must contain a known bit which differs from a known bit
+            in self.min. This would imply that other is not in self.
+        """
+
+        maskOverlap = other.range & ~self.range
+        knownBits = other.min & ~self.range
+
+        return And((maskOverlap == 0), (self.min == knownBits))
 
     
 
@@ -130,16 +172,8 @@ def hma(acc, value, mask):
         mask >>= 1
         value <<= 1
     return acc
-    
-"""
-t1 = tnum(BitVecVal(2, 64), BitVecVal(1, 64))
-t2 = tnum(BitVecVal(2, 64), BitVecVal(0, 64))
-t3 = tnum(BitVec('min', 64), BitVec('range', 64))
 
-solve(t3.eq_tnum(t1.add(t2)))
-solve(t3.eq_tnum(t1.sub(t2)))
-"""
-
+#""""
 #Alright, the cool test
 n1 = tnum(BitVec('n1', 8), BitVecVal(0, 8))
 n2 = tnum(BitVec('n2', 8), BitVecVal(0, 8))
@@ -149,24 +183,35 @@ t1 = tnum(BitVec('tn1_value', 8), BitVec('tn1_mask', 8))
 t2 = tnum(BitVec('tn2_value', 8), BitVec('tn2_mask', 8))
 t3 = tnum(BitVec('tn3_value', 8), BitVec('tn3_mask', 8))
 
+
 solver = Solver()
 
-solver.add(n3.eq_tnum(n1.add(n2)))
+solver.add(n3.tnum_eq(n1.add(n2)))
 
-while solver.check() == sat:
-  m = solver.model()
-  print (m)
+#"""
+solver.add(t1.tnum_in(n1))
+solver.add(t2.tnum_in(n2))
+solver.add((t3.tnum_in(n3)))
+solver.add(t3.tnum_eq(t1.add(t2)))
+solver.add(n1.validate())
+solver.add(n2.validate())
+solver.add(n3.validate())
+solver.add(t1.validate())
+solver.add(t2.validate())
+solver.add(t3.validate())
 
-    
-"""          
-s.add(t1.tnum_in(n1))
-s.add(t2.tnum_in(n2))
-s.add(Not(t3.tnum_in(n3))
-
-s.add(t3.tnum_eq(t1.add(t2)))
 """
+n1 = 10000011 = 131
+n2 = 11111100 = 252
+n3 = 01111111 = 127
 
-#test = tnum(BitVec('val', 64), BitVecVal(0, 64))
-#print(t1.tnum_in(t2))
-#solve(t3.eq_tnum(t1.mult(t2)))
-    
+
+t1 = 10000011 | 01100000 = 132 | 96
+t2 = 11111100 | 00000000 = 252 | 0
+t3 = 00011111 | 11100000 =  31 | 224
+""
+
+
+if solver.check() == sat:
+   m = solver.model()
+   print(m)

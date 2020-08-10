d8a7138a61a78efe1dd2ae10905148cbc4afcc37
Josh Cooper <joshcoop1089@gmail.com>
2020-08-10 11:30:55 -0400

    Phi Functions Algo Completed, strange output possible

--- Josh Code Tests/Code for Next Meeting/SSA_from_BPF.py
+++ Josh Code Tests/Code for Next Meeting/SSA_from_BPF.py
@@ -16,6 +16,7 @@ class Node_Info:
         self.paths_to_node = []
         self.dominated_by = set()
         self.dominance_frontier_nodes = set()
+        self.phi_func = ""
         
         # Getting the specifics from an instruction
         split_ins = instruction.split(" ")        
@@ -33,16 +34,18 @@ class Node_Info:
     def __str__(self):
         print(f'Node {self.node_number}')
         print(f'Instruction: {self.full_instruction}')
-        print(f'Direct Pred Nodes: {self.pred_nodes}')
-        print(f'Pred node Set: {self.pred_set}')
+        print(f'Source: {self.input_value}\tTarget: {self.target_reg}')
+        # print(f'Direct Pred Nodes: {self.pred_nodes}')
+        # print(f'Pred node Set: {self.pred_set}')
         # print(f'Paths to Node: {len(self.paths_to_node)}')
         # for path in self.paths_to_node:
         #     print("New Path:")
         #     for node in path:
         #         print(node, end = " ")
         #     print()    
-        print(f'Dominator Nodes: {self.dominated_by}')
+        # print(f'Dominator Nodes: {self.dominated_by}')
         print(f'Dominance Frontier: {self.dominance_frontier_nodes}')
+        print(f'{self.phi_func}')
         return ""
    
 # This class represents a directed graph using adjacency list representation 
@@ -118,12 +121,11 @@ def node1_sdom_node2_check(node1, node2):
     
     ie, return True if node1 is in the dominator set of node 2, and node1 == node 2
     """
-    return (node1.node_number in node2.dominated_by) and (node1.node_number != node2.node_number)
-    
-    
+    return (node1.node_number in node2.dominated_by) and (node1.node_number != node2.node_number)  
     
 def find_dominance_frontier(node_list):
     """
+    From slide 20 in lecture7.ppt in Code for Next Meeting
     Dominance Frontier Algorithm:
         DF(node D) = {N | There exists a node Pred, which is a predecessor of node N such that
                  node D dominates node Pred, and node d doesn't strictly dominate node N'}
@@ -132,8 +134,10 @@ def find_dominance_frontier(node_list):
         So after establishing path, we should also take the union of the paths to be searched
         
     """
+    # Finding the dominance frontier of checking_node (node D)
     for checking_node in node_list:
 
+        # possible_DF_Node  is node N
         for possible_DF_node in node_list:
             
             if not node1_sdom_node2_check(checking_node, possible_DF_node):
@@ -144,9 +148,40 @@ def find_dominance_frontier(node_list):
     return node_list
 
 # Do the whole Phi Function Algo
-
+def phi_function_locations(node_list):
+    """
+    From slide 26 in lecture7.ppt in Code for Next Meeting
+    """
+    for register_number in range(10):
+        work_list = set()
+        ever_on_work_list = set()
+        already_has_phi_func = set()
+        
+        # Get all nodes which assign a value to our target_reg
+        for node_number, node in enumerate(node_list):
+            if "jmp" not in node.keyword and node.target_reg == register_number:
+                work_list.add(node_number)
+                
+        ever_on_work_list = work_list
+        while len(work_list) != 0:
+            node_to_add_phi_func_to = work_list.pop()
+            for dom_front_node in node_list[node_to_add_phi_func_to].dominance_frontier_nodes:
+                
+                # Insert at most 1 phi function per node
+                if "jmp" not in node_list[dom_front_node].keyword and dom_front_node not in already_has_phi_func:
+                    node_list[dom_front_node].phi_func = f'Need a phi func for register {register_number}'
+                    already_has_phi_func.add(dom_front_node)
+                    
+                    # Process each node at most once
+                    if dom_front_node not in ever_on_work_list:
+                        work_list.add(dom_front_node)
+                        ever_on_work_list.add(dom_front_node)
+                        
+    return node_list
+                        
+                        
 start_time = time.time()
-instruction_list = ["a 1 1", "b 1 2", "jmp 1 2 2", "c 2 1", "jmp 1 1 2", "d 1 2", "e 2 2", "f 2 2"]
+instruction_list = ["a 1 1", "b 1 2", "jmp 1 2 2", "c 2 1", "jmp 1 1 2", "d 1 1", "e 2 2", "f 2 2"]
 # for _ in range(4):
 #     instruction_list.extend(instruction_list)
 # print(len(instruction_list))
@@ -154,6 +189,7 @@ node_list = [Node_Info(instruction, number) for number, instruction in enumerate
 node_list = find_all_edges(node_list)
 node_list = establish_dominators(node_list)
 node_list = find_dominance_frontier(node_list)
+node_list = phi_function_locations(node_list)
 for node in node_list:
     print(node)
 end_time = time.time()
